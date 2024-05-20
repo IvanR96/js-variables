b2cb00f8da2a943f847966ad3455e23bc34bdf5f
IvanR96 <irios96@hotmail.com>
2024-05-19 23:48:53 -0700

    added code

--- script.js
+++ script.js
@@ -5,11 +5,9 @@
 //alert("You have written " + message.length + " characters and you have "  + charLeft +  " characters left in your post");
 
 
-let message = prompt("Enter a message with a max of 250 characters: ");
+// Done in one line
+//alert(prompt("Enter a message with a max of 250 characters: ").slice(0, 250));
 
-let messageUnder250 = message.slice(0, 250);
-
-alert(messageUnder250);
 
 
 
