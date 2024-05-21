35dcf5663ae3b7f31717ab170a01d2d902f63c75
IvanR96 <irios96@hotmail.com>
2024-05-20 20:00:32 -0700

    dog age converter completed

--- script.js
+++ script.js
@@ -9,7 +9,20 @@
 //alert(prompt("Enter a message with a max of 250 characters: ").slice(0, 250));
 
 
+//let name = prompt("What is your name?");
 
 
+//let rest = name.slice(1,name.length);
+
+//name = name.toUpperCase().slice(0,1);
 
 
+//alert("Hello " + name + rest.toLowerCase())
+
+let dogAge = prompt("What is your dog's age?");
+
+
+let humanAge = (dogAge - 2) * 4 + 21;
+
+
+alert("Your dog is " + humanAge + " years old in human years");
