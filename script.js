a85efeb6ed8e58725fb99d8bea389a8cf5ab123d
IvanR96 <irios96@hotmail.com>
2024-05-21 23:24:50 -0700

    added more js code

--- script.js
+++ script.js
@@ -25,4 +25,4 @@ let dogAge = prompt("What is your dog's age?");
 let humanAge = (dogAge - 2) * 4 + 21;
 
 
-alert("Your dog is " + humanAge + " years old in human years");
+alert("Your dog is " + humanAge + " years old in human years. Amazing!");
