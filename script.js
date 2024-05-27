485513af4473749177cd6b2e7305d4e4d4c2f32d
IvanR96 <irios96@hotmail.com>
2024-05-26 18:35:42 -0700

    guestlist exercise done

--- script.js
+++ script.js
@@ -29,24 +29,65 @@
 
 // js functions continue here
 
-
+/*
 function lifeInWeeks(age) {
     
     /************Don't change the code above************/    
         
         //Write your code here.
-        const days = (90 - age) * 365;
+        //const days = (90 - age) * 365;
         
-        const weeks = (90 - age) * 52;
+        //const weeks = (90 - age) * 52;
         
-        const months = (90 - age) * 12;
+        //const months = (90 - age) * 12;
         
-        console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+       // console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
         
         
         
         
     /*************Don't change the code below**********/
-    }
-    
-    lifeInWeeks(56);
+//}
+
+/*let randomNum = Math.random();
+
+ randomNum = randomNum * 100;
+
+ randomNum = Math.floor(randomNum);
+
+console.log(randomNum);
+*/
+
+/*prompt("What is your name?");
+prompt("What is their name?");
+
+let loveScore = Math.random();
+
+loveScore = loveScore * 100;
+
+loveScore = Math.round(loveScore) + 1;
+
+
+if (loveScore > 70){
+    alert("You are " + loveScore + "% compatible! You love eachother like Kanye loves Kanye.");
+
+}
+
+else{
+    alert("You are " + loveScore + "% compatible! Not perfect but still a chance.");
+
+}
+*/
+
+/*let guest = prompt("what is your name?");
+
+let guestList = ["Angela", "Jack", "Pam", "James", "Lara", "Jason"];
+
+if (guestList.includes(guest)){
+    alert("You are an invited guest. Right this way!")
+}
+
+else{
+    alert("You are not invited. Sorry!");
+}
+*/
