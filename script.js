881afbd750279f6e97f4ede84017972a2ca6c4d8
IvanR96 <irios96@hotmail.com>
2024-05-27 23:38:28 -0700

    99 bottles challenge

--- script.js
+++ script.js
@@ -121,3 +121,24 @@ function fizzBuzz(){
     console.log(output);
 }
 */
+
+let bottles = 99;
+
+function bottlesSong(){
+    while(bottles >= 0){
+
+        if (bottles > 0){
+            console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.");
+            bottles--;
+            console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.")
+            
+
+        }
+
+        if (bottles === 0){
+            console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+         }
+
+    }
+
+}
